d1b512c3b58da64c46860f35204480febb843da7
luisadiasn <nsdluaa@gmail.com>
2026-06-12 10:14:45 -0300

    Add files via upload

--- pjt1.py
+++ pjt1.py
@@ -1,20 +1,52 @@
-from random import sample
+def criar_tabela():
+    tabela = []
+    for i in range(3):
+        linha = []
+        for j in range(3):
+            linha.append(" ")
+        tabela.append(linha)
+    return tabela
 
-print('		Campo Minado')
-nome=input('Seu nome: ').lower()
-while True:
-    medida=int(input('Medida do campo: '))
-    if medida>=2 and medida<=9:
-        m=medida*medida
-        n_bombas=int(input(f'N de bombas ({medida}-{m}): '))
-        if n_bombas>=medida and n_bombas<=m:
-            linha=[]
-            for i in range (1,medida+1):
-                linha.append(i)
-            print('#',*linha)
-            for j in range(1,medida+1):
-                print(j,'■ '*medida)
-        break
-linha,coluna:input('L C: ').split()
+def desenhar_tabela(pos: list, tabela: list):
+    pass
+
+def receber_resposta(resp: list, tabela: list):
+    pass
+
+def organizar_resposta(resp: str):
+    respfinal = []
+    for i in resp:
+        respfinal.append(i)
+    respfinal.sort()
+    respfinal[0] = int(respfinal[0]) - 1
+    letra = respfinal[1]
+    if letra.lower() == "a":
+        respfinal[1] = 0
+    if letra.lower() == "b":
+        respfinal[1] = 1
+    if letra.lower() == "c":
+        respfinal[1] = 2
+    return respfinal
+
+def exibir_tabuleiro(tab):
+        print("    A   B   C")
+        print(f"1   {tab[0][0]}  | {tab[0][1]}  | {tab[0][2]}")
+        print("   ---+---+---")
+        print(f"2   {tab[1][0]}  | {tab[1][1]}  | {tab[1][2]}")
+        print("   ---+---+---")
+        print(f"3   {tab[2][0]}  | {tab[2][1]}  | {tab[2][2]}")
+        
+def placar():
+    
+
+caractere=input().upper()
+if caractere=='X' or caractere=='O':
+    tabuleiro=[
+        ['','',''],
+        ['','',''],
+        ['','','']
+        ]
 
 
+x = "A4"
+print(organizar_resposta(x))
