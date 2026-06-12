8a6dd7e6e356a9e434481dc9205ff6724d0ee5c9
Hollow Flower <146781055+hollow-flower@users.noreply.github.com>
2026-06-12 09:15:56 -0300

    resposta organizada

--- trabalhojogodavelha.py
+++ trabalhojogodavelha.py
@@ -1,13 +1,16 @@
 def criar_tabela():
     tabela = []
     for i in range(3):
+        linha = []
         for j in range(3):
-            tabela.append("V")
+            linha.append("V")
+        tabela.append(linha)
+    return tabela
 
-def desenhar_tabela(pos: list):
+def desenhar_tabela(pos: list, tabela: list):
     pass
 
-def receber_resposta(resp: list):
+def receber_resposta(resp: list, tabela: list):
     pass
 
 def organizar_resposta(resp: str):
@@ -15,6 +18,14 @@ def organizar_resposta(resp: str):
     for i in resp:
         respfinal.append(i)
     respfinal.sort()
+    respfinal[0] = int(respfinal[0]) - 1
+    letra = respfinal[1]
+    if letra.lower() == "a":
+        respfinal[1] = 0
+    if letra.lower() == "b":
+        respfinal[1] = 1
+    if letra.lower() == "c":
+        respfinal[1] = 2
     return respfinal
 
 
